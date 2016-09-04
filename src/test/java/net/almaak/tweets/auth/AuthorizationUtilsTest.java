4f7657b8f0b40b438f69673ab493ca652f1524f9
leiferksn <leiferksn@zoho.com>
2016-09-05 01:25:53 +0200

    bug fixes in authorization header class (change in test)

--- src/test/java/net/almaak/tweets/auth/AuthorizationUtilsTest.java
+++ src/test/java/net/almaak/tweets/auth/AuthorizationUtilsTest.java
@@ -1,10 +1,15 @@
 package net.almaak.tweets.auth;
 
+import net.almaak.tweets.utils.TimeUtils;
 import net.almaak.tweets.utils.auth.AuthorizationUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,7 +42,7 @@ public class AuthorizationUtilsTest {
 
     @Before
     public void setUp(){
-        requestParameters.put("user_id", "drwhom");
+        requestParameters.put("user_id", "almaak");
     }
 
     @Test
@@ -48,9 +53,39 @@ public class AuthorizationUtilsTest {
             ACCESS_TOKEN_SECRET,
             requestParameters,
             HTTP_METHOD_GET,
-            httpRequestBaseURL);
+            httpRequestBaseURL, TimeUtils.retrieveTimeOffsetToServer());
 
         Assert.assertNotNull(authorizationHeader);
     }
 
+    @Test
+    public void shouldReturnResponseFromTwitter() throws Exception {
+        String authorizationHeader = AuthorizationUtils.generateSingleUserAuthorizationHeader(CONSUMER_KEY,
+                CONSUMER_SECRET,
+                ACCESS_TOKEN,
+                ACCESS_TOKEN_SECRET,
+                requestParameters,
+                HTTP_METHOD_GET,
+                httpRequestBaseURL, TimeUtils.retrieveTimeOffsetToServer());
+
+        URL url = new URL(httpRequestBaseURL);
+        HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+        con.setRequestMethod(HTTP_METHOD_GET);
+        con.setRequestProperty("Host", "api.twitter.com");
+        con.setRequestProperty("User-Agent", "testing my application");
+        con.setRequestProperty("Authorization", authorizationHeader);
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        Assert.assertNotNull(con.getResponseMessage());
+        Assert.assertTrue(con.getResponseCode() != 401);
+
+        StringBuffer buf = new StringBuffer();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String line = "";
+        while((line = reader.readLine()) != null) {
+            buf.append(line + "\r\n");
+        }
+        String response = buf.toString();
+        Assert.assertTrue(!response.contains("errors"));
+    }
 }
